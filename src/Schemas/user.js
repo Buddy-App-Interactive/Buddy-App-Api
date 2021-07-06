cc676ba238a8b88661f76144bd13cf5670ac293d
Raphael Burgstaller <scodusoft@gmail.com>
2021-07-06 12:37:16 +0200

    fix: sockets add: additional payloads

--- src/Schemas/user.js
+++ src/Schemas/user.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
+let mood = {
+  HAPPY: 1,
+  SAD: 2,
+  OK: 3,
+};
+
 const user = new Schema({
   email: {type: String, default: null},
   password: {type: String, default: null},
   username: {type: String, default: null},
   loginKey: {type: String, default: null},
+  mood: {type: mood, default: 1}
 });
 
 module.exports = mongoose.model('User', user);
--- src/controllers/ChatController.js
+++ src/controllers/ChatController.js
@@ -1,5 +1,6 @@
 const {Chat, Message} = require('../Schemas/index.js');
 var ObjectId = require('mongoose').Types.ObjectId;
+var dateFormat = require('dateformat');
 
 class ChatController {
     fetchChats = async (req, res) => {
@@ -10,16 +11,29 @@ class ChatController {
             .populate("user_from", "username")
             .populate("user_to", "username")
 
-        let map = result.map(item => {
+
+        let map = await Promise.all(result.map( async item => {
             const container = {};
+
+            let message = await Message.findOne({chat: item._id}).sort('-created')
+                .populate([{path:'sender', select:'username _id'}])
+            let container_mesage = {}
+            container_mesage._id = message._id;
+            container_mesage.username = message.sender.username;
+            container_mesage.content = message.content.toString();
+            container_mesage.chatId = message.chat;
+            container_mesage.senderId = message.sender._id
+            container_mesage.created = dateFormat(message.created, "dd/mm/yyyy HH:MM");
 
             container._id = item._id;
             container.username = item.user_to.username===currentUser.username?
                 item.user_from.username:item.user_from.username;
+            container.mood = item.user_from.mood
+            container.lastMessage = container_mesage
             container.requestId = item.chat_request;
 
             return container;
-        })
+        }));
         res.send(map
         );
     };
@@ -44,8 +58,9 @@ class ChatController {
             container._id = item._id;
             container.username = item.sender.username;
             container.content = item.content.toString();
-            container.chatId = item.chat_id;
+            container.chatId = item.chat;
             container.senderId = item.sender._id
+            container.created = dateFormat(result.created, "dd/mm/yyyy HH:MM");
 
             return container;
         })
@@ -72,6 +87,7 @@ class ChatController {
         container.content = result.content.toString();
         container.chatId = req.headers.chat_id;
         container.senderId = result.sender._id
+        container.created = dateFormat(result.created, "dd/mm/yyyy HH:MM");
 
         let chat = await Chat.findOne({_id: chatId})
         let id = (chat.user_to.toString() === currentUser._id.toString()
